45b82805af259bde16176492ce5359e0d0d851aa
faldea1 <faldea1@uc.cl>
2023-01-18 04:23:15 +0000

    avance endpoints en app

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, FavCharacter, FavPlanet
 #from models import Person
 
 app = Flask(__name__)
@@ -31,19 +31,126 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+
+# [GET] /people
+@app.route('/people', methods=['GET'])
 def handle_hello():
+    people_query = Character.query.all()
+    people_list = list(map(lambda x: x.serialize(), people_query))
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Hello, this is your [GET] /people response ",
+        "characters": people_list
     }
 
     return jsonify(response_body), 200
+
+
+# [GET] /people/<int:people_id>
+@app.route('/people/<int:id>', methods=['GET'])
+def get_character():
+    character = Character.query.get(id).serialize()
+    print(character)
+
+    response_body = {
+        "msg": "Hello, this is your [GET] /people/<int:people_id> response ",
+        "character": character
+    }
+
+    return jsonify(response_body), 200
+
+
+# [GET] /planets
+@app.route('/planets/', methods=['GET'])
+def get_planets():
+    planets_query = Planet.query.all()
+    planets_query = list(map(lambda x: x.serialize(), planets_query))
+    print(planets_query)
+
+    response_body = {
+        "msg": "Hello, this is your [GET] /planets response ",
+        "planets": planets_query
+    }
+
+    return jsonify(response_body), 200
+
+
+# [GET] /planets/<int:planet_id>
+@app.route('/planet/<int:id>', methods=['GET'])
+def get_planet():
+    planet = Planet.query.get(id)
+    if planet is None:
+        return "No register of planet", 404
+    planet = planet.serialize()
+    print(planet)
+
+    response_body = {
+        "msg": "Hello, this is your [GET] /planet/<int:planet_id> response ",
+        "planet": planet
+    }
+
+    return jsonify(response_body), 200
+
+
+#[GET] /users
+@app.route('/users/', methods=['GET'])
+def get_users():
+    users_query = User.query.all()
+    users_query = list(map(lambda x: x.serialize(), users_query))
+    print(users_query)
+
+    response_body = {
+        "msg": "Hello, this is your #[GET] /users response ",
+        "users": users_query
+    }
+
+    return jsonify(response_body), 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
